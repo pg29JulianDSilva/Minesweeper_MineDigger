df37bbe85d00b2872d56ef1a5244b7c4e05d420a
neondsilvam <pg29julians@vfs.com>
2025-10-24 00:53:36 -0500

    Recent version repair

--- libraries/gameLogic.js
+++ libraries/gameLogic.js
@@ -1,32 +1,14 @@
-import Difficult from "../modules/dificulty.js";
 import minesweeper from "../modules/Board.js";
 
 class GameEngine {
 
-    constructor(Egame, Mgame, Hgame) {
-        this.difficulty = new Difficult();
-        this.dNumber = this.difficulty.setNumber();
-        this.board = new minesweeper(Egame, Mgame, Hgame);
+    constructor(Egame, Mgame, Hgame, dChosen) {
+        this.board = new minesweeper(Egame, Mgame, Hgame, dChosen);
         
-    }
-
-    setDificulty(gSelected, otherGame1, otherGame2,) {
-        return this.difficulty.dificulty(gSelected, otherGame1, otherGame2, this.board.reStart(this.difficulty.setNumber()));;
-    }
-    setDificultyNumber() {
-        return this.dNumber;
     }
 
     setNumies() {
         return this.board.rNumies();
-    }
-
-    callRestart() {
-        return this.board.reStart(this.dNumber);
-    }
-
-    setMines() {
-        return this.board.rFlags();
     }
 }
 
